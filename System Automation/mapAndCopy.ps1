978fc3700d1646ab89ed61119c7792afb6a56e29
Songwen Xue <owensit98@gmail.com>
2020-04-29 15:15:57 -0500

    On branch master

---
+++ System Automation/mapAndCopy.ps1
@@ -0,0 +1,13 @@
+﻿#*******************************************README*******************************************************
+#this PS script is used for mapping a network drive and copy file from there to you local drive         *
+#prerequisites: 1. the network path for the remote drive. 2. the absolute path for the destination path.*
+#By: Songwen Xue                                                                                        *
+#********************************************************************************************************
+
+$Network_Path = "\\192.168.0.13\Share Folder"
+Write-Host "your provided newwork path is $Network_Path" -ForegroundColor Red
+New-SmbMapping -LocalPath * -RemotePath "$Network_Path" -Confirm | Select-String -Pattern 'LocalPath'
+Remove-SmbMapping  z:, Y: -Confirm
+Get-PSDrive | Select-Object Root |  {Select-String -Pattern}
+
+
