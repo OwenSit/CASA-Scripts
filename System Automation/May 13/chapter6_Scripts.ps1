edc18cb097cdc8f51bdcf26e6788a815e45d3863
Songwen Xue <owensit98@gmail.com>
2020-05-13 12:33:02 -0500

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   System Automation/May 13/chapter6_Scripts.ps1 	new file:   System Automation/May12/DiffServices 	new file:   System Automation/May12/chapter4_Scripts.ps1 	new file:   System Automation/May12/longDiff1.txt 	new file:   System Automation/May12/longDiff2.txt 	new file:   System Automation/May12/longDiffResult.txt 	new file:   System Automation/May12/myFile 	new file:   System Automation/May12/service 	new file:   System Automation/May12/services.csv 	new file:   System Automation/May12/shortDiff1.txt 	new file:   System Automation/May12/shortDiff2.txt 	new file:   System Automation/May12/shortDiffResult.txt

---
+++ System Automation/May 13/chapter6_Scripts.ps1
@@ -0,0 +1,48 @@
+﻿#chapter 6
+
+#1
+#Identify a cmdlet that will produce a random number
+Get-Command "*random*" #look for command that contain the character random
+Get-Random #then I found this command
+
+#2
+#Identify a cmdlet that will display the current date and time.
+Get-Command "*date*"
+Get-Date
+
+#3
+#What type of object does the cmdlet from task #2 produce? (What is the type
+#name of the object produced by the cmdlet?)
+Get-Date | Get-Member
+#answer: It is a System.DateTime type object
+
+#4
+#Using the cmdlet from task #2 and Select-Object, display only the current day
+#of the week in a table
+Get-Date | Select-Object DayOfWeek
+
+#5
+#Identify a cmdlet that will display information about installed hotfixes
+Get-Command "*hotfix*"
+Get-HotFix
+
+#6
+#Using the cmdlet from task #5, display a list of installed hotfixes. Sort the list by
+#the installation date, and display only the installation date, the user who
+#installed the hotfix, and the hotfix ID.
+Get-HotFix | Select-Object HotFixID, InstalledOn, InstalledBy | Sort-Object -Descending InstalledOn
+
+#7
+#Repeat task #6, but this time sort the results by the hotfix description, and
+#include the description, the hotfix ID, and the installation date. Put the results
+#into an HTML file.
+Get-HotFix | Select-Object HotFixID, InstalledOn, Description | Sort-Object Description
+
+#8
+#Display a list of the 50 newest entries from the Security event log (you can use a
+#different log, such as System or Application, if your Security log is empty). Sort
+#the list so that the oldest entries appear first, and so that entries made at the
+#same time are sorted by their index. Display the index, time, and source for
+#each entry. Put this information into a text file (not an HTML file, just a plain
+#text file).
+Get-EventLog Security -Newest 50 | Select-Object index, TimeWritten, source | Sort-Object TimeWritten, index | Out-File -FilePath C:\Users\Administrator\Desktop\output.txt
---
+++ System Automation/May12/chapter4_Scripts.ps1
@@ -0,0 +1,78 @@
+﻿#chapter 4
+
+#1
+#Create a CliXML reference file for the services on your computer. Then, change
+#the status of some non-essential service like BITS (stop it if it’s already started;
+#start it if it’s stopped on your computer). Finally, use Diff to compare the reference CliXML file to the current state of your computer’s services. You’ll need to
+#specify more than the Name property for the comparison—does the -property
+#parameter of Diff accept multiple values? How would you specify those multiple values?
+Import-Clixml C:\Users\Administrator\Desktop\service # place the clixml file of the services on desktop
+Get-Service "*BITS*" #search for command named BITS
+Start-Service -Name "BITS" # start it if it's stopped
+#compare differce of service status after starting BITS, store output in the DiffService file
+diff -ReferenceObject (Import-Clixml C:\Users\Administrator\Desktop\service) -DifferenceObject (Get-Service) -Property Name, Status > C:\Users\Administrator\Desktop\DiffServices
+
+
+#2
+#Create two similar, but different, text files. Try comparing them using Diff. To
+#do so, run something like this: Diff -reference (Get-Content File1.txt)
+#-difference (Get-Content File2.txt). If the files have only one line of text
+#that’s different, the command should work. If you add a bunch of lines to one
+#file, the command may stop working. Try experimenting with the Diff command’s -syncWindow parameter to see if you can get the command working
+#again.
+
+#example for short difference
+diff -ReferenceObject (Get-Content C:\Users\Administrator\Desktop\shortDiff1.txt) -DifferenceObject (Get-Content C:\Users\Administrator\Desktop\shortDiff2.txt) > C:\Users\Administrator\Desktop\shortDiffResult.txt
+
+#example for long difference (which is still working lol)
+diff -ReferenceObject (Get-Content C:\Users\Administrator\Desktop\longDiff1.txt) -DifferenceObject (Get-Content C:\Users\Administrator\Desktop\longDiff2.txt) > C:\Users\Administrator\Desktop\longDiffResult.txt
+
+
+#3
+#What happens if you run Get-Service | Export-CSV services.csv | Out-File
+#from the console? Why does that happen?
+
+#answer: a services.csv file will be generated and no object will flow into the Out-File command
+
+#4
+#Apart from getting one or more services and piping them to Stop-Service,
+#what other means does Stop-Service provide for you to specify the service or
+#services you want to stop? Is it possible to stop a service without using GetService at all?
+
+#answer: we can also use the -name parameter to specify the name of the service that we want to stop
+
+
+#5
+#What if you wanted to create a pipe-delimited file instead of a comma-separated
+#file? You would still use the Export-CSV command, but what parameters would
+#you specify?
+
+#answer: we can use the -Delimiter parameter in the Export-CSV command
+
+#6
+#Is there a way to eliminate the # comment line from the top of an exported CSV
+#file? That line normally contains type information, but what if you wanted to
+#omit that from a particular file?
+
+#answer: we can use the -NoTypeInformation parameter in the Export-CSV command
+
+#7
+#Export-CliXML and Export-CSV both modify the system, because they can create and overwrite files. What parameter would prevent them from overwriting
+#an existing file? What parameter would ask you if you were sure before proceeding to write the output file?
+
+#answer: we can use the -Append parameter to prevent both Export-CliXml and Export-CSV command from overwriting the output files
+#        the -Confirm parameter will prompt us to confirm the procedures
+
+#8
+#Windows maintains several regional settings, which include a default list separator. On U.S. systems, that separator is a comma. How can you tell Export-CSV to
+#use the system’s default separator, rather than a comma?
+(Get-Culture).TextInfo.ListSeparator # this command is used to check out what is the system's default separator
+
+#answer: we can use the -UseCulture parameter to separate the data with the system's default separator 
+
+
+
+
+
+
+
